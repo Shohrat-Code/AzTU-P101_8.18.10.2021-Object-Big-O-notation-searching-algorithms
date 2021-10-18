b11f585ed5ab1e2741650c08e9bd6278ba50cd13
Shohrat-Code <shohrat@code.edu.az>
2021-10-18 12:10:06 +0400

    Codes

---
+++ ObjectBigONotation/ObjectBigONotation/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ObjectBigONotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student stu1 = new Student
+            {
+                Name = "Rasim",
+                Surname = "Abbasov",
+                Age = 25,
+                IsMarried = false,
+                Height = 175
+            };
+
+            //stu1.Fullinfo();
+
+            Student stu2 = new Student()
+            {
+                Name = "Qasim",
+                Surname = "Agayev",
+                Age = 32,
+                IsMarried = true
+            };
+
+            Student stu3 = new Student()
+            {
+                Name = "Tofiq",
+                Surname = "Quliyev",
+                Age = 18,
+                IsMarried = false
+            };
+
+            //stu2.Fullinfo();
+
+            Student[] students = { stu1, stu2, stu3 };
+
+            for (int i = 0; i < students.Length; i++)
+            {
+                Console.WriteLine(students[i].Name + " - " + students[i].Height);
+            }
+
+
+        }
+    }
+}
---
+++ ObjectBigONotation/ObjectBigONotation/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectBigONotation
+{
+    class Student
+    {
+        public int Height = 15;
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public byte Age { get; set; }
+        public bool IsMarried { get; set; }
+
+        public void Fullinfo()
+        {
+            Console.WriteLine(this.Name + " " + this.Surname + " - " + this.Age);
+        }
+    }
+}
